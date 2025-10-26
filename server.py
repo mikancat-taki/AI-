241342e402dc3bb1769202bfa65b22e942783d27
mikancat10 <216382990+mikancat-taki@users.noreply.github.com>
2025-10-26 18:40:48 +0900

    Update server.py

--- server.py
+++ server.py
@@ -6,7 +6,7 @@ from ai.model_transformer import TransformerAI
 app = Flask(__name__)
 
 # インスタンス化
-markov_ai = MarkovAI()
+model_markov_ai = MarkovAI()
 transformer_ai = TransformerAI()
 
 @app.route('/')
